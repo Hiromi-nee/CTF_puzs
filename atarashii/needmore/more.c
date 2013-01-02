1b45efdd08842c1b959b284fa199f2c6213ea803
Kanoe Hiromi <kyou.wk@gmail.com>
2013-01-02 22:55:47 +0800

    changes

--- atarashii/needmore/more.c
+++ atarashii/needmore/more.c
@@ -3,16 +3,19 @@
 
 int main(int argc, char *argv[]){
   char* derp = "onetwo";
-  char herp[200]; //not o9k :(
+  char herp[1000]; //not o9k :(
+  unsigned int d[1000] = {0x00};
+  d[100] = 3735941133u;
+  d[101] = 3405705229u;
   int offset = 1;
   strcpy(herp, argv[2]);
   if(argc != strlen(derp)){
     printf("Wrong\n");
     exit(0);
   }else{
-    if(argc != (strlen(derp)+offset)){
-      printf(argv[0]);
-      printf("damn that was not easy!\n");
+    if(argc == (strlen(derp))){
+      printf(argv[1]);
+      printf("damn that was easy!\n");
     }else{
       if(offset != 1){
 	setreuid(1000, 1000);
