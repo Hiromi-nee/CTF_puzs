06c1bb0b3daad10962d021128a0379f0d22a5994
Hiromi-nee <kyou.wk@gmail.com>
2013-01-02 21:05:32 +0800

    fix

--- atarashii/needmore/more.c
+++ atarashii/needmore/more.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char *argv[]){
   char* derp = "onetwo";
+  char herp[20];
+  int offset = 1;
+  strcpy(herp, argv[2]);
   if(argc != strlen(derp)){
     printf("Wrong\n");
     exit(0);
   }else{
-    if(argc != (strlen(derp)+1)){
+    if(argc != (strlen(derp)+offset)){
       printf(argv[0]);
-      printf("damn that was easy!\n");
+      printf("damn that was not easy!\n");
     }else{
       printf(argv[1]);
       printf("damn that was easy!\n");
